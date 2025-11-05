791d30aabbe7abc2bd82fbaf58bb75bee7bf97d2
Hasan5352 <hasanlokhandwala27@gmail.com>
2025-11-05 16:08:03 +0100

    routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import { Routes, Route } from 'react-router'
 import './App.css'
-import { HomePage } from './pages/HomePage'
+import { HomePage } from './pages/HomePage/HomePage';
+import { CheckoutPage } from './pages/CheckoutPage/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage/TrackingPage';
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path='checkout' element={<CheckoutPage />} />
+      <Route path='orders' element={<OrdersPage />} />
+      <Route path='tracking' element={<TrackingPage />} />
+    </Routes>
   )
 }
 
