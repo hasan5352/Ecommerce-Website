e56136160da5dfa6b09cdeeaa0b6c5eac45254ea
Hasan5352 <hasanlokhandwala27@gmail.com>
2025-11-06 02:47:41 +0100

    get products from backend

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,20 +1,30 @@
+import { useState, useEffect, use } from 'react';
 import './HomePage.css';
 import { Header } from '../../components/Header';
 import { Product } from './Product';
 import { products } from '../../../data/products';
 
 export function HomePage(){
+	const [allProducts, setAllProducts] = useState([]);
+
+	async function fetchProducts() {
+		let prods = await fetch("http://localhost:3000/api/products");
+		prods = await prods.json();
+		setAllProducts(prods);
+	}
+	useEffect(()=>{ fetchProducts(); }, []);
+	
 	return (
 		<>
 			<Header />
 
 			<div className="products-grid home-page">
 
-				{products.map((p) => (
+				{allProducts.map((p) => (
 					<Product productImg={p.image}
 						title={p.name}
 						ratingImg={`images/ratings/rating-${p.rating.stars * 10}.png`}
-						reviews={p.rating.count} price={p.priceCents / 100}
+						reviews={p.rating.count} price={p.priceCents / 100} key={p.id}
 					/>
 				))}
 			
