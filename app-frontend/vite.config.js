42a8dc4fcd2def029e6f05e81dc75608c7be1097
Hasan5352 <hasanlokhandwala27@gmail.com>
2026-02-12 21:18:20 +0100

    Added Env variable in vite.config.js for seemlessly connecting api proxies to the backend

--- app-frontend/vite.config.js
+++ app-frontend/vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const env = loadEnv('', process.cwd(), '')
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: { '/api' : { target: "http://localhost:3000" } }
+    proxy: { '/api' : { target: env.VITE_API_URL } },
+    changeOrigin: true
   },
   build: {
-    sourcemap: false,
-    outDir: '../app-backend/dist'
+    outDir: 'dist'
   }
 
 })
